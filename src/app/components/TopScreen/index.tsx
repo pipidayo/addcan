ab6313dcbc4871f95746144e3ae5c903c155f7c4
pipidayo <sussu5515@gmail.com>
2025-04-03 22:39:32 +0900

    topscreen2

--- src/app/components/TopScreen/index.tsx
+++ src/app/components/TopScreen/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 export default function TopScreen() {
+  const router = useRouter()
+  const [name, setName] = useState('')
+  const [roomCode, setRoomCode] = useState('')
+
   return <div>TopScreen</div>
 }
